cc6eb7ea9ea08efbf13cd8abe566af8f719ce2ac
T. Lanette <TLPollard4516@gmail.com>
2020-07-12 15:27:59 -0400

    Added beginning of js
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ 2-counter/setup/app.js
@@ -0,0 +1,8 @@
+// set intial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
